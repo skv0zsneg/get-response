b3672cddc2463aa8ea9408c68276005f3ef90cd7
skvozsneg <1_0_91@bk.ru>
2022-05-19 00:56:16 +0300

    [add] Add tests for .get_objects() method.

--- get_response/parsers.py
+++ get_response/parsers.py
@@ -1,10 +1,10 @@
 from abc import ABC
 from abc import abstractmethod
 import json
-from typing import Any, Optional, List
+from typing import Any, Tuple
 
 
-T_GET_OBJECTS = List[Optional[Any]]
+T_GET_OBJECTS = Tuple[Any]
 
 
 class BaseParser(ABC):
@@ -14,19 +14,18 @@ class BaseParser(ABC):
         self.obj = obj
 
     @abstractmethod
-    def get_objects(self, item: str, obj: Any = None) -> T_GET_OBJECTS:
+    def get_objects(self, field: str) -> T_GET_OBJECTS:
         """Method parsing the object and return what he found by given
         key.
 
-        Method get object and trying to found in it value that exist in
-        it by the given key.
+        Method get object and trying to found in it values that exist
+        in it by the given key.
 
         Args:
-            obj: Object that will be parsed.
-            item: String - field which value must be found.
+            field: Field which value must be found.
 
         Returns:
-            List[Optional[Any]]: Method return a found values or None if 
+            Tuple[Any]: Method return a found values or empty tuple if 
              searching are failed. 
         """
         pass
@@ -38,25 +37,23 @@ class JsonParser(BaseParser):
         super().__init__(obj)
         self._obj_dict = json.loads(self.obj)
 
-    def get_objects(self, field: str, obj: dict = None) -> T_GET_OBJECTS:
-        # TODO: избавиться от рекурсии!!!
+    def get_objects(self, field: str) -> T_GET_OBJECTS:
+        """Parsing a serialized to dict json object."""
+        dicts_queue = [self._obj_dict,]
         fields_found = []
 
-        for key, value in self._obj_dict.items():
+        for dictionary in dicts_queue:
+            for key, value in dictionary.items():
 
-            if key == field:
-                fields_found.append(value)
+                if key == field:
+                    fields_found.append(value)
 
-            elif isinstance(value, dict):
-                results = self.get_objects(field, value)
-                for result in results:
-                    fields_found.append(result)
+                elif isinstance(value, dict):
+                    dicts_queue.append(value)
 
-            elif isinstance(value, list):
-                for item in value:
-                    if isinstance(item, dict):
-                        more_results = self.get_objects(field, item)
-                        for another_result in more_results:
-                            fields_found.append(another_result)
-
-        return fields_found
+                elif isinstance(value, list):
+                    for item in value:
+                        if isinstance(item, dict):
+                            dicts_queue.append(item)
+        
+        return tuple(fields_found)
--- tests/test_get_objects.py
+++ tests/test_get_objects.py
@@ -6,7 +6,7 @@ from get_response import get_response
 class TestGetJsonParsed:
 
     @pytest.fixture()
-    def json_message(self):
+    def parse_result(self):
         message = """
         {
             "page": 20,
@@ -39,27 +39,68 @@ class TestGetJsonParsed:
             ]
         }
         """
-        return message
-
-    def test_get_all_id_values(self, json_message):
         parsed_json = get_response(
-            obj=json_message, 
+            obj=message, 
             message_type='json')
 
-        all_ids_result = parsed_json.get_objects('id')
+        yield parsed_json
+
+    def test_get_all_id_values(self, parse_result):
+        all_ids_result = parse_result.get_objects('id')
         
         assert len(all_ids_result) == 3
-        assert all_ids_result == ['abfa0c8a986bcabca9ba6c96a9', 
+        assert all_ids_result == ('abfa0c8a986bcabca9ba6c96a9', 
                                   'abfa0c8a986bcabca9ba890bbc', 
-                                  'abfa0c8a986bcabca9ba890123']
+                                  'abfa0c8a986bcabca9ba890123')
 
-    def test_get_all_name_values(self, json_message):
-        parsed_json = get_response(
-            obj=json_message, 
-            message_type='json')
-
-        all_ids_result = parsed_json.get_objects('name')
+    def test_get_all_name_values(self, parse_result):
+        all_ids_result = parse_result.get_objects('name')
         
         assert len(all_ids_result) == 3
-        assert all_ids_result == ['Alex', 'Vikki', 'Lily']
+        assert all_ids_result == ('Alex', 'Vikki', 'Lily')
+    
+    def test_get_all_surname_values(self, parse_result):
+        all_surname_result = parse_result.get_objects('surname')
         
+        assert len(all_surname_result) == 3
+        assert all_surname_result == ('Bernard', 'Sweet', 'Sunshine')
+    
+    def test_get_all_n_childrens_values(self, parse_result):
+        all_n_childrens_result = parse_result.get_objects('n_childrens')
+        
+        assert len(all_n_childrens_result) == 3
+        assert all_n_childrens_result == (15165136521356416541651, 0, 2)
+    
+    def test_get_all_married_values(self, parse_result):
+        all_married_result = parse_result.get_objects('married')
+        
+        assert len(all_married_result) == 3
+        assert all_married_result == (False, True, True)
+
+    def test_get_all_books_values(self, parse_result):
+        all_books_result = parse_result.get_objects('books')
+        
+        assert len(all_books_result) == 3
+        assert all_books_result == (["The Great Gatsby", "Another Book"],
+                                    None, 
+                                    [])
+    
+    def test_get_page_value(self, parse_result):
+        page_result = parse_result.get_objects('page')
+
+        assert page_result == (20,)
+
+    def test_get_time_value(self, parse_result):
+        page_result = parse_result.get_objects('time')
+
+        assert page_result == ('2022-05-15T15:43:00Z',)
+
+
+    def test_get_value_using_nonexistent_field(self, parse_result):
+        parsing_result = parse_result.get_objects('nonexistent')
+
+        assert parsing_result == ()
+
+    def test_get_value_using_nonexistent_field(self, parse_result):
+        with pytest.raises(TypeError):
+            parse_result.get_objects()
