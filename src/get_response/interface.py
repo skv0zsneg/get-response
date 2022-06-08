7e650da7f8e0c794a76435568aaf285698f780bd
skvozsneg <1_0_91@bk.ru>
2022-06-08 22:19:15 +0300

    [add && upd] Add test Sphinx documentation.

--- src/get_response/interface.py
+++ src/get_response/interface.py
@@ -8,6 +8,15 @@ T_MESSAGE = Literal['json', 'soap']
 
 
 def get_response(obj: str, message_type: T_MESSAGE) -> BaseParser:
+    """
+    Return a list of random ingredients as strings.
+
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :raise lumache.InvalidKindError: If the kind is invalid.
+    :return: The ingredients list.
+    :rtype: list[str]
+    """
     parser_factory = {
         'json': JsonParser,
         'soap': SoapParser,
